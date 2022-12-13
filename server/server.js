2bd069718afd447205403e34f35aceec45e03a17
Daphne <71684146+duckysheep@users.noreply.github.com>
2022-12-13 17:24:23 +0800

    deployment setup

---
+++ server/server.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+console.log("Mongo_URI", MONGO_URI);
+mongoose.set("debug", true);
+mongoose.set("runValidators", true);
+mongoose.set("strictQuery", false);
+mongoose.connect(MONGO_URI);
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.static("../client/dist"));
+
+app.get("/api", (req, res) => {
+  res.json({ msg: "Hello World!" });
+});
+
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve("../client/dist", "index.html"))
+);
+
+mongoose.connection.once("open", () => {
+  console.log("connected to mongoose...");
+  app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+  });
+});
