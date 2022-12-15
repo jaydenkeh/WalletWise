5f684807ac6aab1b28ea37f88e3743ea00a6f6da
Daphne <71684146+duckysheep@users.noreply.github.com>
2022-12-15 10:49:26 +0800

    resolve local main conflict

--- server/server.js
+++ server/server.js
@@ -4,9 +4,9 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const userController = require("./controllers/userController.js");
 const Income = require("./models/Income.js");
 const incomeController = require("./controllers/incomeController.js");
-const userController = require("./controllers/userController.js");
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -22,9 +22,9 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.static("../client/dist"));
+app.use("/api/signup", userController);
 
 app.use("/api/income", incomeController);
-app.use("/api/signup", userController);
 
 app.get("/api", (req, res) => {
   res.json({ msg: "Hello World!" });
