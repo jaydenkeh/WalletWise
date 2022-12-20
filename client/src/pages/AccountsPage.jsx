d3947f500421223b000739ace1b57f6ceed79e9c
Jayden Keh <zhaoxikeh@gmail.com>
2022-12-20 11:02:35 +0800

    added accounts and transactions pages

---
+++ client/src/pages/AccountsPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NavigationBar from "../components/NavigationBar";
+
+export default function AccountsPage() {
+  return (
+    <>
+      <NavigationBar />
+      <div>AccountsPage</div>
+    </>
+  );
+}
--- server/server.js
+++ server/server.js
@@ -21,12 +21,6 @@ mongoose.connect(MONGO_URI);
 
 app.use(express.json());
 app.use(cors());
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
 app.use(morgan("dev"));
 app.use(express.static("../client/dist"));
 app.use("/api/signup", signupController);
